3940a4fdad9f6577bd2121d56eede42c29657e99
Alex Brockson <alexbrockson@gmail.com>
2023-12-30 21:26:49 -0600

    12/30

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -1,27 +1,126 @@
+type Node<T> = {
+    value: T,
+    prev?: Node<T>,
+    next?: Node<T>,
+}
+
 export default class DoublyLinkedList<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = this.tail = undefined;
     }
 
     prepend(item: T): void {
+        const newNode = { value: item } as Node<T>;
+        this.length++;
+        if (!this.head) {
+            this.head = this.tail = newNode;
+            return;
+        }
 
-}
+        newNode.next = this.head;
+        this.head.prev = newNode;
+        this.head = newNode;
+    }
+
     insertAt(item: T, idx: number): void {
+        if (idx > this.length) {
+            throw new Error("Index out of bounds");
+        }
+        if (idx === 0) {
+            this.prepend(item);
+        }
+        if (idx === this.length) {
+            this.append(item);
+        }
+        
+        const newNode = { value: item } as Node<T>;
+        this.length++;
 
-}
+        let currNode = this.head;
+        for (let i = 0; i < idx; i++) {
+            currNode = currNode!.next;
+        }
+
+        const prevNode = currNode!.prev;
+        prevNode!.next = newNode;
+        newNode.prev = prevNode;
+        currNode!.prev = newNode;
+        newNode.next = currNode;
+    }
+    
     append(item: T): void {
+        if (this.length === 0) {
+            this.prepend(item);
+            return;
+        }
 
-}
-    remove(item: T): T | undefined {
+        const newNode = { value: item } as Node<T>;
+        this.length++;
 
-}
+        newNode.prev = this.tail;
+        this.tail!.next = newNode;
+        this.tail = newNode;
+    }
+
     get(idx: number): T | undefined {
+        if (idx >= this.length) {
+            throw new Error("Index out of bounds");
+        }
+        if (idx === 0) {
+            return this.head!.value;
+        }
+        if (idx === this.length - 1) {
+            return this.tail!.value;
+        }
 
-}
+        let currNode = this.head;
+        for (let i = 0; i < idx; i++) {
+            currNode = currNode?.next;
+        }
+        
+        return currNode!.value
+    }
+
+    remove(item: T): T | undefined {
+        let current = this.head;
+        while (current) {
+            if (current.value === item) {
+                this.removeNode(current);
+    
+                this.length--;
+                return current.value;
+            }
+            current = current.next;
+        }
+        return undefined;
+    }
+
     removeAt(idx: number): T | undefined {
+        if (idx >= this.length) {
+            throw new Error("Index out of bounds");
+        }
+    
+        let current = this.head;
+        for (let i = 0; i < idx; i++) {
+            if (current?.next) current = current?.next;
+        }
+    
+        this.removeNode(current!);
+    
+        this.length--;
+        return current!.value;
+    }
+    
+    private removeNode(current: Node<T>) {
+        if (current.prev) current.prev.next = current.next;
+        else this.head = current.next;
 
-}
+        if (current.next) current.next.prev = current.prev;
+        else this.tail = current.prev;
+    }
 }
--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,148 @@
-export default function quick_sort(arr: number[]): void {
+function qs(arr: number[], lo: number, hi: number): void {
+    // Base case:
+    if (lo >= hi) {
+        return;
+    }
 
+    const pivotIdx = partition(arr, lo, hi);
+
+    qs(arr, lo, pivotIdx - 1);
+    qs(arr, pivotIdx + 1, hi);
 }
+
+function partition(arr: number[], lo: number, hi: number): number {
+    const pivot = arr[hi];
+    let idx = lo - 1;
+
+    for (let i = lo; i < hi; i++) {
+        if (arr[i] <= pivot) {
+            idx++;
+            const tmp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = tmp;
+        }
+    }
+
+    idx++;
+    arr[hi] = arr[idx];
+    arr[idx] = pivot;
+
+    return idx;
+}
+
+export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length - 1);
+}
+
+// how it works:
+// function partition(arr: number[], lo: number, hi: number): number {
+//     const pivot = arr[hi];
+//     let idx = lo - 1;
+
+//     for (let i = lo; i < hi; i++) {
+//         if (arr[i] <= pivot) {
+//             idx++;
+//             const tmp = arr[i];
+//             arr[i] = arr[idx];
+//             arr[idx] = tmp;
+//         }
+//     }
+
+//     idx++;
+//     arr[hi] = arr[idx];
+//     arr[idx] = pivot;
+
+//     return idx;
+// }
+
+
+// BEGIN:
+
+// [2, 1, 8, 5, 34, 3, 4, [6]]
+// idx = -1 
+
+
+// [(2), 1, 8, 5, 34, 3, 4, [6]]
+// i = 0
+// arr[0] = 2;
+// idx = -1
+// 2 < [6]
+// idx = 0
+// tmp = 2
+// arr[idx] = 2
+// arr[0] = 2
+// unchanged
+
+
+// [2, (1), 8, 5, 34, 3, 4, [6]]
+// i = 1
+// arr[1] = 1;
+// idx = 0
+// 1 < [6]
+// idx = 1
+// tmp = 1
+// arr[idx] = 1
+// arr[1] = 1
+// unchanged
+
+
+// [2, 1, (8), 5, 34, 3, 4, [6]]
+// i = 2
+// arr[2] = 8;
+// idx = 1
+// 8 > [6] !!!
+// idx = 1
+// unchanged
+
+
+// [2, 1, 8, (5), 34, 3, 4, [6]]
+// i = 3
+// arr[3] = 5;
+// idx = 1
+// 5 < [6]
+// idx = 2
+// tmp = 5
+// arr[3] = arr[2]
+// (5 swap with 8)
+// arr[2] = 5
+// [2, 1, 5, 8, 34, 3, 4, [6]]
+
+
+// [2, 1, 5, 8, (34), 3, 4, [6]]
+// i = 4
+// arr[4] = 34;
+// idx = 2
+// 34 > [6] !!!
+// idx = 2
+// unchanged
+
+
+// [2, 1, 5, 8, 34, (3), 5, [6]]
+// i = 5
+// arr[5] = 3;
+// idx = 2
+// 3 < [6]
+// idx = 3
+// tmp = 3
+// arr[5] = arr[3]
+// (3 swap with 8)
+// arr[3] = 3
+// [2, 1, 5, 3, 34, 8, 5, [6]]
+
+
+// [2, 1, 5, 3, 34, 8, (5), [6]]
+// i = 6
+// arr[6] = 5;
+// idx = 3
+// 5 < [6]
+// idx = 4
+// tmp = 5
+// arr[6] = arr[4]
+// (5 swap with 34)
+// arr[4] = 5
+// [2, 1, 5, 3, 5, 8, 34, [6]]
+
+
+// idx = 6;
+// arr[7] = arr[6] (arr[7] = 34)
+// arr[6] = 6;
