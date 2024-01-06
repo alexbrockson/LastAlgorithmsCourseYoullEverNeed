be35e9b1414e0b0ab212f857ba8d9bf702460871
Alex Brockson <alexbrockson@gmail.com>
2024-01-05 20:28:33 -0600

    Trees

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,21 @@
 export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+    // Base Cases
+    // structural check
+    if (a === null && b === null) {
+        return true;
+    }
 
+    // structural check
+    if (a === null || b === null) {
+        return false;
+    }
+
+    // value check
+    if (a.value !== b.value) {
+        return false;
+    }
+
+
+    // Recurse
+    return compare(a.left, b.left) && compare(a.right, b.right);
 }
